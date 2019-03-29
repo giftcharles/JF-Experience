3d8e84a8ab18e80f81a66c014bf804747def3345
giftcharles <mcrider45g@gmail.com>
2019-03-29 19:36:15 +0300

    Add Light theme initial css

--- src/bg/background.js
+++ src/bg/background.js
@@ -8,6 +8,6 @@
 //example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
+    console.log(sender.tab.id);
     sendResponse();
   });
